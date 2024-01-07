59494f42d30e831ddf66d1d721ddf419d40798ef
Jun <ayz1070@gmail.com>
2024-01-08 08:41:44 +0900

    240108

---
+++ src/C3_DataStructure/P1546.java
@@ -0,0 +1,24 @@
+package C3_DataStructure;
+
+import java.util.Scanner;
+
+public class P1546 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] scores = new int[n];
+        for(int i =0 ;i<n; i++){
+            scores[i] = sc.nextInt();
+        }
+
+        double sum = 0;
+        int max = 0;
+
+        for(int i : scores){
+            sum += i;
+            max = Math.max(max,i);
+        }
+        System.out.println(sum*100/max/n);
+    }
+}
