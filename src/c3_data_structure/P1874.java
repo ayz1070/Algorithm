5c964acb788b081ada16d15d3cb2fa0b7116b6b6
ayz1070 <ayz1070@gmail.com>
2025-01-23 22:24:13 +0900

    [백준] 1874 문제 풀이

---
+++ src/c3_data_structure/P1874.java
@@ -0,0 +1,43 @@
+package c3_data_structure;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class P1874 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] A = new int[n];
+        for(int i=0; i<n; i++){
+            A[i] = sc.nextInt();
+        }
+        Stack<Integer> stack = new Stack<>();
+        StringBuffer bf = new StringBuffer();
+
+        int num = 1;
+        boolean result = true;
+        for(int i = 0; i<A.length; i++){
+            int s = A[i];
+
+            if(s >= num){
+                while(s >= num){
+                    stack.push(num++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
+            }else{
+                int k = stack.pop();
+
+                if(k>s){
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                }else{
+                    bf.append("-\n");
+                }
+            }
+        }
+        if(result) System.out.println(bf.toString());
+    }
+}
