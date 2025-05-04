9eaa32e17db38ccef11c36e2963670887cd81a8f
ayz1070 <ayz1070@gmail.com>
2025-05-05 07:57:47 +0900

    [백준] 11720 문제 풀이

---
+++ src/c3_data_structure/P11720_2.java
@@ -0,0 +1,19 @@
+package c3_data_structure;
+
+import java.util.Scanner;
+
+public class P11720_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String nStr = sc.next();
+        int sum = 0;
+
+        for(int i = 0; i<n; i++){
+            sum += nStr.charAt(i) -'0';
+            //sum += Integer.parseInt(String.valueOf(nStr.charAt(i)));
+        }
+
+        System.out.println(sum);
+    }
+}
