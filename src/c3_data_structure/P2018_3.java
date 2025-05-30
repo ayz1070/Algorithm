1fa531f263f9d351a544f27d484e58fc4910ddd3
ayz1070 <ayz1070@gmail.com>
2025-05-30 20:56:05 +0900

    [백준] 2018 문제 풀이

---
+++ src/c3_data_structure/P2018_3.java
@@ -0,0 +1,30 @@
+package c3_data_structure;
+
+import java.util.Scanner;
+
+public class P2018_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] s = new int[n+1];
+        s[0] = 0;
+        for(int i =1; i<n+1; i++){
+            s[i] = s[i-1] + i;
+        }
+
+        int count = 1;
+        int start = 1;
+        int end = 1;
+        while(end<=n){
+            if(s[end]-s[start]<n){
+                end++;
+            }else if(s[end]-s[start]>n){
+                start++;
+            }else if(s[end]-s[start]==n){
+                count++;
+                start++;
+            }
+        }
+        System.out.println(count);
+    }
+}
