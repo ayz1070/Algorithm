7c1113e971e556a7b851458be9a0ddbede2d3d3a
ayz1070 <ayz1070@gmail.com>
2025-05-31 22:52:04 +0900

    [백준] 1546 문제 풀이

---
+++ src/c3_data_structure/P1546_3.java
@@ -0,0 +1,27 @@
+package c3_data_structure;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class P1546_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double n = sc.nextInt();
+        List<Double> m = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            m.add(sc.nextDouble());
+        }
+
+        double max = Collections.max(m);
+        int sum = 0;
+
+        for(int i = 0; i < n; i++) {
+            sum += m.get(i);
+        }
+        System.out.println(sum*100/(max*n));
+    }
+}
