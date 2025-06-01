8b25ba44da95a0ea3a7753ae46808b78074b5265
ayz1070 <ayz1070@gmail.com>
2025-06-01 18:07:52 +0900

    [백준] 11659 문제 풀이

---
+++ src/c3_data_structure/P11659_3.java
@@ -0,0 +1,35 @@
+package c3_data_structure;
+
+import java.util.Scanner;
+
+public class P11659_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] sums = new int[n];
+
+        for(int i =0; i<n; i++){
+            int num = sc.nextInt();
+            if(i==0){
+                sums[i] = num;
+            }else{
+                sums[i] = sums[i-1] + num;
+            }
+
+        }
+
+
+        for(int k =1; k<=m; k++){
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+
+            if(i==1){
+                System.out.println(sums[j-1]);
+            }else{
+                System.out.println(sums[j-1]-sums[i-2]);
+            }
+        }
+    }
+}
